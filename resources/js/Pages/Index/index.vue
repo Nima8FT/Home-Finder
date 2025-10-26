95bd39d09fc73796fc6f80f3ddc90d05db8eca7e
Nima8FT <nima_8ak@yahoo.com>
2025-10-26 14:57:42 +0330

    create structure - Home Finder project with sail

---
+++ resources/js/Pages/Index/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+import {ref} from 'vue'
+
+const counter = ref(0)
+
+setInterval(() => counter.value++ , 1000)
+</script>
+
+<template>
+    <h1 class="font-bold">Hi nima {{ counter }} </h1>
+</template>
